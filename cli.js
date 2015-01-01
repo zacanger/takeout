c89c53613ef3d0e492766c0c514b29a0029e6b6e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-01 20:39:15 +0900

    support `timeout` option on CLI

--- cli.js
+++ cli.js
@@ -9,6 +9,7 @@ var argv = require('minimist')(process.argv.slice(2), {
     o: 'out',
     e: 'encoding',
     d: 'index',
+    t: 'timeout',
     h: 'help',
     v: 'version'
   },
@@ -16,29 +17,33 @@ var argv = require('minimist')(process.argv.slice(2), {
   boolean: ['help', 'version']
 });
 
-var pkg = require('./package.json');
-
 if (argv.version) {
-  console.log(pkg.version);
+  console.log(require('./package.json').version);
 
 } else if (argv.help || argv._.length === 0) {
   var chalk = require('chalk');
+  var cyan = chalk.cyan;
+  var gray = chalk.gray;
+  var yellow = chalk.yellow;
+
+  var pkg = require('./package.json');
 
   console.log([
-    chalk.cyan(pkg.name) + chalk.gray(' v' + pkg.version),
+    cyan(pkg.name) + gray(' v' + pkg.version),
+    gray('https://github.com/shinnn/takeout'),
     pkg.description,
-    chalk.gray('https://github.com/shinnn/takeout'),
     '',
     'Usage1: ' + pkg.name + ' <file path | URL> --out <dest>',
     'Usage2: ' + pkg.name + ' <file path | URL> > <dest>',
     '',
     'Options:',
-    chalk.yellow('--out,      -o <file path>') + '  Write the result to a file (stdout by default)',
-    chalk.yellow('--encoding, -e <encoding> ') + '  Set encoding (same as Node\'s encoding option)',
-    chalk.yellow('--index,    -d <filename> ') + '  Set the filename of directory index',
-    chalk.yellow('--no-index,               ') + '  Do not care about directory index',
-    chalk.yellow('--help,     -h            ') + '  Print usage information',
-    chalk.yellow('--version,  -v            ') + '  Print version',
+    yellow('--out,      -o <file path> ') + '  Write the result to a file (stdout by default)',
+    yellow('--encoding, -e <encoding>  ') + '  Set encoding (same as Node\'s encoding option)',
+    yellow('--index,    -d <filename>  ') + '  Set the filename of directory index',
+    yellow('--no-index,                ') + '  Do not care about directory index',
+    yellow('--timeout,  -t <ms>        ') + '  Set time after which the request will be aborted',
+    yellow('--help,     -h             ') + '  Print usage information',
+    yellow('--version,  -v             ') + '  Print version',
     ''
   ].join('\n'));
 
@@ -53,17 +58,27 @@ if (argv.version) {
     options.directoryIndex = argv.index;
   }
 
+  options.timeout = argv.timeout;
+
   var takeout = require('./');
   takeout(argv._[0], options, function(err, body) {
-    if (err) {
-      throw err;
+    if (!err) {
+      if (argv.out) {
+        var outputFileSync = require('output-file-sync');
+        try {
+          outputFileSync(argv.out, body);
+        } catch (e) {
+          err = e;
+        }
+      } else {
+        process.stdout.write(body);
+      }
     }
 
-    if (argv.out) {
-      var outputFileSync = require('output-file-sync');
-      outputFileSync(argv.out, body);
-    } else {
-      process.stdout.write(body);
+    if (err) {
+      process.stderr.write(err.stack + '\n', function() {
+        process.exit(1);
+      });
     }
   });
 }
